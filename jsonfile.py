5d32d85ea1115de8955dab53de19debf22568d43
haam458 <hedyk@hkhk.edu.ee>
2025-03-12 15:17:32 +0200

    uus

--- jsonfile.py
+++ jsonfile.py
@@ -1,4 +1,13 @@
 import json
-with open('failid/products.json', 'r',encoding='utf-8') as file:
+
+with open("products.json", "r",encoding="utf-8") as file:
     products = json.load(file)
-    
+for products in products:
+    lipstick = products["lipstick"]
+    price = products["price"]
+
+    print(f"Toode: {lipstick}")
+    print(f"Hind: {price}")
+    #print("-" * 40)
+    #print(type(products))
+    #print(products)
