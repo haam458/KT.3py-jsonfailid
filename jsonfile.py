0896edc5e1d9d31ea98aebda183516621990dc89
haam458 <hedyk@hkhk.edu.ee>
2025-05-23 21:05:04 +0300

    Update jsonfile.py

--- jsonfile.py
+++ jsonfile.py
@@ -6,16 +6,13 @@ response = requests.get(url)
 
 if response.status_code == 200:
     products_data = response.json()
+
     for products in products_data['products']:
         print(f"Hind: {products['price']}\nToode: {products['brand']}\n")
+
+
 else:
     print("Päring ebaõnnestu, staatuskood:", response.status_code)
 
 products = input("Millist toodet soovite: ")
 
-for products in products['products']:
-    if products['toode'].lower() == products.lower():
-        print(f"{products} on {products['price']} €")
-        break
-if not products:
-   print(f"{products} ei ole.")   
